19a6db8eac50a33a240cc8c52ac3dd84478826ab
kaspares <kacper240804@gmail.com>
2026-03-05 11:55:27 +0100

    feat: implement PortfolioItem services

---
+++ PortfolioTracker.Application/Interfaces/IPortfolioItemService.cs
@@ -0,0 +1,12 @@
+﻿using PortfolioTracker.Application.DTOs;
+
+namespace PortfolioTracker.Application.Interfaces;
+
+public interface IPortfolioItemService
+{
+    Task<IEnumerable<PortfolioItemDto>> GetAllByPortfolio(Guid portfolioId);
+    Task<PortfolioItemDto> GetByIdAsync(Guid id);
+    Task CreateAsync(CreatePortfolioItemDto dto, Guid portfolioId);
+    Task UpdateAsync(UpdatePortfolioItemDto dto, Guid portfolioId);
+    Task DeleteAsync(Guid portfolioId);
+}
---
+++ PortfolioTracker.Application/Services/PortfolioItemService.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using PortfolioTracker.Application.DTOs;
+using PortfolioTracker.Application.Interfaces;
+using PortfolioTracker.Domain.Entities;
+using PortfolioTracker.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortfolioTracker.Application.Services
+{
+    public class PortfolioItemService(ILogger<PortfolioItemService> logger,
+        IMapper mapper,
+        IPortfolioItemRepository portfolioItemRepository) : IPortfolioItemService
+    {
+        public async Task CreateAsync(CreatePortfolioItemDto dto, Guid portfolioId)
+        {
+            logger.LogInformation("Creating new portfolio item {@dto} in portfolio with id: {@PortfolioId}", dto, portfolioId);
+            
+            var portfolioItem = mapper.Map<PortfolioItem>(dto);
+            portfolioItem.PortfolioId = portfolioId;
+
+            await portfolioItemRepository.AddAsync(portfolioItem);
+
+        }
+
+        public async Task DeleteAsync(Guid portfolioId)
+        {
+            logger.LogInformation("Deleting portfolio with id: {@portfolioId}", portfolioId);
+            var portfolioDto = await GetByIdAsync(portfolioId);
+
+            var portfolio = mapper.Map<PortfolioItem>(portfolioDto);
+
+            await portfolioItemRepository.DeleteAsync(portfolio);
+        }
+
+        public async Task<IEnumerable<PortfolioItemDto>> GetAllByPortfolio(Guid portfolioId)
+        {
+            logger.LogInformation("Getting all portfolioItems for portfolio with id: {@portfolioId}", portfolioId);
+            var portfolios = await portfolioItemRepository.GetAllPortfolioItemInPortfolio(portfolioId);
+
+            return mapper.Map<IEnumerable<PortfolioItemDto>>(portfolios);
+
+        }
+
+        public async Task<PortfolioItemDto> GetByIdAsync(Guid id)
+        {
+            logger.LogInformation("Getting portfolioItem with id: {@id}", id);
+            var portfolioItem = await portfolioItemRepository.GetPortfolioItemByIdAsync(id)
+                ?? throw new Exception("Portfolio item not found");
+
+            return mapper.Map<PortfolioItemDto>(portfolioItem);
+        }
+
+        public Task UpdateAsync(UpdatePortfolioItemDto dto, Guid portfolioId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
